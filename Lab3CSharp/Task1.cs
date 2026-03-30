f65e1ccc1857a89a73afcaca9b5e05d194301c55
BatsuraBohdana <batsura.bohdana@chnu.edu.ua>
2026-03-30 19:13:12 +0300

    Fix formatting of triangle array initialization

--- Lab3CSharp/Task1.cs
+++ Lab3CSharp/Task1.cs
@@ -12,10 +12,10 @@ namespace Lab3CSharp
             // Створюємо масив з 4 трикутників
             Triangle[] triangles = new Triangle[]
             {
-                new Triangle(3, 4, 5, 10),  // Колір 10
-                new Triangle(10, 10, 12, 5),// Колір 5
-                new Triangle(6, 8, 10, 1),  // Колір 1
-                new Triangle(5, 5, 5, 8)    // Колір 8
+                new Triangle(3, 4, 5, 10),  
+                new Triangle(10, 10, 12, 5),
+                new Triangle(6, 8, 10, 1), 
+                new Triangle(5, 5, 5, 8)   
             };
 
             Console.WriteLine("\n1. Впорядковані за КОЛЬОРОМ:");
